0575fc3dbb2fdce211fca15e49c50f990aaba25d
AHMED-JUNAID <37082978+AHMED-JUNAID@users.noreply.github.com>
2018-03-08 17:54:30 +0500

    interfaces in c#

--- StudentProfile/StudentProfile/Program.cs
+++ StudentProfile/StudentProfile/Program.cs
@@ -7,92 +7,34 @@ using System.IO;
 
 namespace StudentProfile
 {
-    //diff b/w class and struct
-    public struct Customer
+    //interface concept
+    interface MY_INTERFACE
     {
-        private int Id;
-        private string Name;
-
-        
-
-        public Customer(int Id, string Name)
-        {
-            this.Id = Id;
-            this.Name = Name;
-        }
-
-        public string Name1
-        {
-            get { return this.Name; }
-            set { this.Name = value; }
-        }
-
-        public int ID
-        {
-            get { return this.Id; }
-            set { this.Id = value; }
-        }
-
-       
+        //int iAge; //cannot contains field
+        //public void print(); //access modifier is not allowed
+        //only declaration is allowed
+        //no definition of member is allowed
+        //by default members are PUBLIC in Interface
+        void print();
     }
 
-    public class Student
+    public class Customer : MY_INTERFACE
     {
-        private int id;
-        private string name;
-
-        public int Id
+        public void print()
         {
-            get { return id; }
-            set { id = value; }
+            Console.WriteLine("interface print method implemented\n\n");
         }
-
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
-
-        ~Student()
-        {
-        }
-        
     }
-
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Customer c1 = new Customer(123, "actual");
-            Customer c2 = c1;
+            MY_INTERFACE m1 = new Customer();
+            m1.print();
 
-            c2.ID = 456;
-            c2.Name1 = "copy";
-
-            //Console.WriteLine(c2.ID);
-            //Console.WriteLine(c2.Name1);
-
-            //Console.WriteLine(c1.ID);
-            //Console.WriteLine(c1.Name1);
-
-            Student s1 = new Student();
-            s1.Id = 101;
-            s1.Name = "class concept";
-
-            Student s2 = new Student();
-            s2 = s1;
-
-            Console.WriteLine(s1.Id);
-            Console.WriteLine(s1.Name);
-
-            s2.Id = 901;
-            s2.Name = "change effected";
-            Console.WriteLine(s2.Id);
-            Console.WriteLine(s2.Name);
-
-            Console.WriteLine(s1.Id);
-            Console.WriteLine(s1.Name);
-
+            Customer c1 = new Customer();
+            c1.print();
         }
     }
 }
